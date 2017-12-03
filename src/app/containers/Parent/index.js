602fab6e90067d5006dabb9213df9f582c0de4f8
aksel <aksel@akseltorgard.com>
2017-12-03 20:40:20 +0100

    Added childroute and route links to Parent. Disabled anchor-is-valid, as the Link component is caught by it.

--- src/app/containers/Parent/index.js
+++ src/app/containers/Parent/index.js
@@ -1,10 +1,59 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router5';
 
-const Parent = () => (
+import childRouteSelector from '../../redux/selectors/childRoute';
+
+const Parent = ({ childRoute }) => (
   <div>
     <h1>Parent</h1>
-    This component has child routes.
+    <em>This component has child routes.</em>
+
+    <div
+      style={{
+        margin: 5,
+        paddingBottom: 8,
+        borderBottom: '4px solid #555',
+      }}
+    >
+      <Link
+        style={{
+          border: '1px solid #222',
+          padding: 5,
+        }}
+        routeName="parent.child"
+      >
+        Child
+      </Link>
+
+      <Link
+        style={{
+          marginLeft: 5,
+          border: '1px solid #222',
+          padding: 5,
+        }}
+        routeName="parent.childParent"
+      >
+        Child with parent
+      </Link>
+    </div>
+
+    <div style={{ padding: 5 }}>
+      { childRoute || <span>This is the default component.</span> }
+    </div>
+
+    {
+      childRoute
+      &&
+      <div style={{ padding: 5 }}>
+        <Link routeName="parent">
+          <em>Back to Parent</em>
+        </Link>
+      </div>
+    }
   </div>
 );
+Parent.propTypes = { childRoute: PropTypes.node };
 
-export default Parent;
+export default connect(childRouteSelector('parent'))(Parent);
